ed62085580aa73a133c908ac5dba2c07c8d519f3
Micheal Limones <Micheal.Limones@du.edu>
2021-08-02 16:02:21 -0600

    removed console.logs

--- assets/script.js
+++ assets/script.js
@@ -63,9 +63,6 @@ var previousSearchResults = function () {
 };
 
 var displayWeather = function (cityName) {
-  console.log(cityName.coord.lon);
-  console.log(cityName.coord.lat);
-
   var uvApiUrl =
     "https://api.openweathermap.org/data/2.5/onecall?&lat=" +
     cityName.coord.lat +
@@ -76,9 +73,7 @@ var displayWeather = function (cityName) {
     "&units=imperial";
 
   var currentWeather = function () {
-    console.log(cityName);
     var currentCityName = cityName.name;
-    console.log(currentCityName);
     cityNameEl.textContent = currentCityName;
 
     var weatherIcon = cityName.weather[0].icon;
@@ -88,23 +83,19 @@ var displayWeather = function (cityName) {
     );
 
     var currentCityTemp = cityName.main.temp;
-    console.log(currentCityTemp);
+
     cityTempEl.textContent = "Temperature: " + currentCityTemp + "";
 
     var currentCityHumidity = cityName.main.humidity;
-    console.log(currentCityHumidity);
     cityHumidityEl.textContent = "Humidity: " + currentCityHumidity + "%";
 
     var currentCityWindSpeed = cityName.wind.speed;
-    console.log(currentCityWindSpeed);
     windSpeedEl.textContent = "Wind Speed: " + currentCityWindSpeed + "MPH";
 
     fetch(uvApiUrl).then(function (response) {
       response.json().then(function (data) {
-        console.log(data);
         var currentUvIndex = data.current.uvi;
-        console.log(currentUvIndex);
-        uvIndexEl.textContent = currentUvIndex;
+        uvIndexEl.textContent = "UV Index: " + currentUvIndex;
         if (currentUvIndex < 3) {
           uvIndexEl.setAttribute("class", "bg-success text-white");
         } else if (currentUvIndex < 6) {
@@ -116,14 +107,12 @@ var displayWeather = function (cityName) {
     });
 
     previousSearches.push(cityName.name);
-    console.log(previousSearches);
     localStorage.setItem("City", JSON.stringify(previousSearches));
   };
   var forcastWeather = function (cityName) {
     fetch(uvApiUrl).then(function (response) {
       response.json().then(function (data) {
         var forcastArray = data.daily;
-        console.log(forcastArray);
 
         for (var i = 1; i < 6; i++) {
           const element = forcastArray[i];
@@ -131,7 +120,7 @@ var displayWeather = function (cityName) {
           var forcastDiv = document.createElement("div");
           forcastDiv.setAttribute(
             "class",
-            "col-3 forcast bg-primary text-white m-2 p-2 rounded"
+            "col-2 forcast bg-primary text-white m-2 p-2 rounded"
           );
 
           var forcastIcon = element.weather[0].icon;
@@ -142,19 +131,16 @@ var displayWeather = function (cityName) {
           );
           forcastDiv.appendChild(forcastIconImg);
 
-          console.log(element.temp.day);
           var forcastTemp = element.temp.day;
           var forcastTempDiv = document.createElement("div");
           forcastTempDiv.textContent = "temp: " + forcastTemp;
           forcastDiv.appendChild(forcastTempDiv);
 
-          console.log(element.wind_speed);
           var forcastWindSpeed = element.wind_speed;
           var forcastWindSpeedDiv = document.createElement("div");
           forcastWindSpeedDiv.textContent = "Wind: " + forcastWindSpeed + "MPH";
           forcastDiv.appendChild(forcastWindSpeedDiv);
 
-          console.log(element.humidity);
           var forcastHumidity = element.humidity;
           var forcastHumidityDiv = document.createElement("div");
           forcastHumidityDiv.textContent = "Humidity: " + forcastHumidity + "%";
